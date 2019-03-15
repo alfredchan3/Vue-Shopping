75c085388abc9bea8fba17069d975b0416d7ca88
Alfred Chen <brilychen@gmail.com>
2019-03-15 13:02:20 +0800

    add some depandencies

--- src/main.js
+++ src/main.js
@@ -2,7 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/router'
 
+import './vant'                   // 有赞组件库
+import FastClick from 'fastclick' // 解决移动端 300ms 点击延时
+FastClick.attach(document.body)
+
+import 'vant/lib/index.css'
+import 'css/reset.css'
+import 'swiper/dist/css/swiper.css'
+import 'css/border.css'
+//import 'css/index.css'
+//import 'css/icon.styl'
+//import 'css/cropper.css'
+
 Vue.config.productionTip = false
+
+Vue.filter('toFixed', function (val) {
+  val = Number(val)
+  return val.toFixed(2)
+})
 
 new Vue({
   router,
